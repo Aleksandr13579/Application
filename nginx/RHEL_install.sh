2fde6d44b1a7d267fd90b66392f2d11d7eb44efa
Aleksandr13579 <al13579qw@yandex.ru>
2024-05-08 12:53:03 +0300

    сщfixed bugs

--- nginx/RHEL_install.sh
+++ nginx/RHEL_install.sh
@@ -3,10 +3,10 @@
 touch /etc/yum.repos.d/nginx.repo;
 
 #add nginx repo
-cat <<EOF > touch /etc/yum.repos.d/nginx.repo
+cat <<EOF > /etc/yum.repos.d/nginx.repo
 [nginx-stable]
 name=nginx stable repo
-baseurl=http://nginx.org/packages/centos/$releasever/$basearch/
+baseurl=http://nginx.org/packages/centos/8/x86_64/
 gpgcheck=1
 enabled=1
 gpgkey=https://nginx.org/keys/nginx_signing.key
@@ -26,8 +26,10 @@ yum update && yum install nginx && nginx -v;
 systemctl enable nginx;
 systemctl start nginx;
 
-#unlock http port in firewall
-firewall-cmd --zone=public --add-service=http
+#unlock http/https port in firewall
+firewall-cmd --zone=public --add-service=http;
+firewall-cmd --zone=public --add-service=https;
+setsebool -P httpd_can_network_connect 1;
 
 #configuring nginx
 chmod 755 configuring.sh;
--- nginx/configuring.sh
+++ nginx/configuring.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 #change kernel configuration
-sudo cp ./sysctl/777-propetries-for-nginx.conf /etc/sysctl.d/ ;
+sudo cp ./sysctl/777-properties-for-nginx.conf /etc/sysctl.d/ ;
 sudo sysctl -p;
 sudo sysctl --system;
 
 #ssl cert generate
 openssl genrsa -out ./ssl/tls.key 2048 && \
 openssl req -new -key ./ssl/tls.key -out ./ssl/tls.csr -config ./ssl/req.conf && \
-openssl req -x509 -sha256 -nodes -new -key ./ssl/key.key -out ./ssl/CAcert.crt -config ./ssl/req.conf && \
+openssl req -x509 -sha256 -nodes -new -key ./ssl/tls.key -out ./ssl/CAcert.crt -config ./ssl/req.conf && \
 openssl x509 -sha256 -CAcreateserial -req -days 365 -in ./ssl/tls.csr -extfile ./ssl/req.conf -CA ./ssl/CAcert.crt -CAkey ./ssl/tls.key -out ./ssl/tls.crt;
 
 
-сp ./ssl/CAcert.crt /etc/ssl;
+cp ./ssl/CAcert.crt /etc/ssl;
 cp ./ssl/tls.key /etc/ssl;
 cp ./ssl/tls.crt /etc/ssl;
 #Configure and start nginx server
